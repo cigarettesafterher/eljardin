cbf1d0f4897bd87b1c9364fcfeab8553f86451ea
cigarettesafterher <101540563+cigarettesafterher@users.noreply.github.com>
2022-07-17 01:49:06 +0200

    borrar popover en movil

--- components/Login.js
+++ components/Login.js
@@ -17,10 +17,10 @@ function Login({ providers }) {
     <div className="flex flex-col items-left space-y-20 pt-10">
   <div className="relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="hidden relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
         
           <Popover>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <div className="hidden relative pt-6 px-4 sm:px-6 lg:px-8">
               <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
